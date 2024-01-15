2b6bfdc7a0d9a4da3474adb0b72a2a524fe7c5b7
Vladislav Matuizo <dalvmatujzo@gmail.com>
2024-01-15 22:00:18 +0100

    Initial commit

---
+++ src/test/java/com/example/breweryconfigserver/BreweryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.breweryconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
